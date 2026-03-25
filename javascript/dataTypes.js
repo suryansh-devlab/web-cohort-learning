66e5d266a1aa6dd33601191e714b7a87fb57cf8b
Suryansh Kushwaha <suryanshkushwaha1235@gmail.com>
2026-03-25 18:42:27 +0530

    Add data types example with variables and object structure

---
+++ javascript/dataTypes.js
@@ -0,0 +1,33 @@
+// Primitive Data Type
+
+// string - "", '', ``
+let name = "Suryansh";
+
+// Number - integer, float, double
+let age = 23;
+
+// Boolean - true, false
+let isPaid = true;
+
+// null - The variable exists but intentionally has no value.
+let favouriteClass = null;
+
+// undefined → variable declared but not assigned
+let hometown;
+
+// Non -Primitive Data Type
+
+//  Object
+let skills = ["HTML", "CSS", "JavaScript"];
+
+// object -> Key value pairs
+let studentProfile = {
+  name: "Surya",
+  age: 23,
+  isPaid: true,
+  skills: [`HTML, CSS, JAVASCRIPT`],
+  favouriteClass: null,
+  hometown,
+};
+
+console.log(typeof age); // typeof -> is used to determinr the which type of data
